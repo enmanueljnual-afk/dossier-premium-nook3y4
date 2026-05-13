e03213c55640b09c0f5c2521586a46fffd76158a
enmanueljnual-afk <enmanuel.j.nual@gmail.com>
2026-05-13 12:29:33 -0300

    Delete Dossier-premium 3y4.Nook directory

--- Dossier-premium 3y4.Nook/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NookPresentation } from "@/components/nook/nook-presentation"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <NookPresentation />
-    </main>
-  )
-}
